6a3516b56d8b5c59d59e4de0d7a055773ec967c4
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-03-19 08:29:36 -0600

    adds mathy stuff to exercise

--- ex5.py
+++ ex5.py
@@ -1,28 +1,38 @@
 # in python, format string == rubys string interpolation. difference is, we use { } instead of #{ }
-my_name = "Zac Palmquist"
+name = "Zac Palmquist"
 
-my_age = 27
+age = 27
 
-my_height = 6.15
+height = 6.15
+height_inch = height * 12
+height_centimeter = round((height_inch * 2.54), 2)
 
-my_weight = 165
+weight = 165
+kilo_convert = 2.20462
+weight_in_kilo = round((165 / kilo_convert), 4)
 
-my_eyes = 'brown'
+eyes = 'brown'
 
-my_teeth = 'white' # on good days
+teeth = 'white' # on good days
 
-my_hair = 'brown'
+hair = 'brown'
 
 # interesting that I must format the string with the letter f beforehand.
-print(f"Lets talk about {my_name}.")
+print(f"Lets talk about {name}.")
 # attempt to see what happens if I don't use the formatting 'f'
-# print("Lets talk about {my_name}.")
-# we get Lets talk about Zac Palmquist. vs Lets talk about {my_name}.
-print(f"He's {my_height} feet tall.")
-print(f"He's {my_weight} pounds heavy.")
+# print("Lets talk about {name}.")
+# we get Lets talk about Zac Palmquist. vs Lets talk about {name}.
+print(f"He's {height} feet tall.")
+print(f"He's {height_centimeter} centimeters tall")
+print(f"He's {weight} pounds heavy.")
+print(f"He weighs {weight_in_kilo} kilos.") # answer should be 74.8427
 print("That's not that heavy")
-print(f"He's got {my_eyes} eyes and {my_hair} hair.")
-print(f"His teeth are colored {my_teeth}, on a good day. Depends on the coffee.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are colored {teeth}, on a good day. Depends on the coffee.")
 
-total = my_age + my_height + my_weight
-print(f"If I add my age: {my_age}, my height: {my_height} and my weight: {my_weight}, I get {total}") # total should be 198.15
+total = age + height + weight
+print(f"If I add my age: {age}, my height: {height} and my weight: {weight}, I get {total}") # total should be 198.15
+
+# challenge
+# Try to write some variables that convert the inches and pounds to centimeters and kilograms. Do
+# not just type in the measurements. Work out the math in Python.
