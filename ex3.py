bcad6659ab652b58c48a0c5248f751f2d9ef526f
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-03-19 07:48:13 -0600

    commiting exercises

---
+++ ex3.py
@@ -0,0 +1,28 @@
+# this is simply printing the string of "I will now count my chickens"
+print("I will now count my chickens:")
+# Python is interesting because it automatically prints the float instead of just an integer
+print("Hens", 25 + 30 / 6)
+# Here it does not print a float, I wonder why.
+print("Roosters", 100 - 25 * 3 % 4)
+# again, just printing the string
+print("Now I will count the eggs:")
+# returns the result (float)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# here its just printing the string
+print("Is it true that 3 + 2 < 5 - 7?")
+# returns a boolean since we are asking "is it less than?"
+print(3 + 2 < 5 - 7)
+# this prints string since its wrapped in quotes, then returns integer since we are doing math
+print("What is 3 + 2?", 3 + 2)
+# this does the same as above
+print("what is 5 - 7?", 5 - 7)
+# printing string
+print("Oh, that's why it's False.")
+# again, printing string
+print("How about some more.")
+# now its returning a boolean value
+print("Is it greater?", 5 > -2)
+# same as above
+print("Is it greater or equal?", 5 >= -2)
+# again, just printing and returning boolean
+print("Is it less or equal?", 5 <= -2)
