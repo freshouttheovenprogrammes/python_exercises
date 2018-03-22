4e68f703e198feb265f06ac20188573159d36d13
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-03-22 15:21:42 -0600

    did stuff

--- ex6.py
+++ ex6.py
@@ -1,18 +1,18 @@
 # setting an integer var
 types_of_people = 10
 # here we are still setting a string var, and using string interpolation with 'f'. No parens needed
-x = f"There are {types_of_people} types of people."
+x = f"There are {types_of_people} types of people." # 1/4
 # two vars set as strings
 binary = "binary"
 do_not = "don't"
 # again, setting a var using string interp
-y = f"Those who know {binary} and those who {do_not}."
+y = f"Those who know {binary} and those who {do_not}." # 2/4
 # now we are printing both vars. that used string interp
 print(x)
 print(y)
 # this showcases printing the vars. parens required as we are actually printing them
-print(f"I said: {x}")
-print(f"I also said: {y}")
+print(f"I said: {x}") # 3/4
+print(f"I also said: {y}") # 4/4
 # setting a var as a Boolean value
 hilarious = False
 # this is interesting. for some reason we include the brackets. if we exclude the brackets the boolean doesn't hit
