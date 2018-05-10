70932aa1f422f183b562e148c10257367c94ba1b
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-05-10 11:48:20 -0600

    more strings and now messing with modules

---
+++ ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print("The script is called:", script)
+first = input("Your first variable is: ")
+second = input("Your second variable is: ")
+third = input("Your third variable is: ")
+print(f"You chose {first}, {second}, {third} ")
