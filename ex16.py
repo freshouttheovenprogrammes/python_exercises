707d620c904242a9a98b157eb16b421b749ee5c5
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-05-10 12:46:28 -0600

    refactor

--- ex16.py
+++ ex16.py
@@ -19,12 +19,8 @@ line3 = input("line 3: ")
 #
 print("I'm going to write these to the file.")
 #
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+new = "\n"
+target.write(line1 + new + line2 + new + line3 + new)
 #
 print("And finally, we close it.")
 target.close() # still a little lost on this front
