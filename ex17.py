365edaa5a8d09ca52a04ce5c57f5adfddea5e3f4
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-05-10 13:53:24 -0600

    one line, wow

--- ex17.py
+++ ex17.py
@@ -1,18 +1,3 @@
-from sys import argv
+from sys import argv; from os.path import exists; script, from_file, to_file = argv; in_file = open(from_file).read(); out_file = open(to_file, 'w').write(in_file); print(f"Copying from {from_file} to {to_file}\nThe input file is {len(in_file)} bytes long\nAlright, all done.")
+
 # what is os.path? what is exists?
-from os.path import exists
-
-script, from_file, to_file = argv
-
-print(f"Copying from {from_file} to {to_file}")
-
-in_file = open(from_file).read()
-
-print(f"The input file is {len(in_file)} bytes long")
-
-out_file = open(to_file, 'w')
-out_file.write(in_file)
-
-print("Alright, all done.")
-
-out_file.close()
