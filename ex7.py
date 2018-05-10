0330feef56301d877e8ec6c0a03f19cf5baf7c1c
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-05-10 10:06:16 -0600

    do ex7 again

--- ex7.py
+++ ex7.py
@@ -1,5 +1,5 @@
 print("Mary had a little lamb.")
-print("Its fleece was white as {}.".format('snow'))
+print("Its fleece was white as {}. ".format('snow'))
 print("And everywhere that Mary went.")
 print("." * 10)
 
@@ -16,6 +16,5 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-# watch that comma at the end. try removing it to see what happens
-print(end1 + end2 + end3 + end4 + end5 + end6, end= ' ')
+print(end1 + end2 + end3 + end4 + end5 + end6, end='')
 print(end7 + end8 + end9 + end10 + end11 + end12)
